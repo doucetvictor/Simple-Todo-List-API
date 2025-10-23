c2c2f7772d8f0eb28f2215b4d1e81dea8b293c20
Victor Doucet <victor.doucet@epitech.eu>
2025-10-23 22:04:13 +0200

    [FIX] wrong response status code in todos get endpoint

--- backend/src/routes/todos/retrieve.js
+++ backend/src/routes/todos/retrieve.js
@@ -7,10 +7,10 @@ router.get('/', async (req, res) => {
     const { id } = req.query;
     if (id) {
         const todoElem = await todoListInst.getTodo(id);
-        res.status(201).send(todoElem);
+        res.status(200).send(todoElem);
     } else {
         const todoList = await todoListInst.getTodoList(id);
-        res.status(201).send(todoList);
+        res.status(200).send(todoList);
     }
 });
 
